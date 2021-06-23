491a5a11830d526e69d428e6c680e30a4fac3d54
William Doyle <mcbidebilly@gmail.com>
2021-06-23 10:44:12 -0400

    actually use weights

--- main.js
+++ main.js
@@ -30,12 +30,12 @@ const types = [
 	{
 		name: 'charm',
 		weights: {
-			up: 100 / 2,
+			up: 100 / 32,
 			down: 100 / 8,
 			charm: 100 / 4,
 			strange: 100 / 16,
 			top: 100 / 64,
-			bottom: 100 / 32,
+			bottom: 100 / 2,
 		}
 	},
 	{
@@ -77,28 +77,21 @@ let devised = [];
 
 function GetNextWithWeights(prevType) {
 	// 1. Get the weights
+	console.log(`prevType is ${prevType}`);
 	const weights = types.find(t => t.name == prevType).weights;
 
 	// 2. pick a value between 0 and 100
 	const selector = Math.floor(Math.random() * 100);
 
 	const keys = Object.keys(weights);
-	//const values = Object.values(weights);
 	const keyValuePairs = keys.map(key => { return { key: key, value: weights[key] } });
 	const sorted = [...keyValuePairs].sort((a, b) => a.value - b.value)
 
 	for (let i = 0; i < sorted.length; i++) {
 		if (selector < sorted[i].value)
-			return sorted[i].name;
+			return sorted[i].key;
 	}
-	return sorted[sorted.length - 1].name
-	//	console.log(keyValuePairs);
-	//	console.log('sorted--');
-	//	console.log(sorted);
-
-	//console.log(selector);
-	//console.log(weights);
-
+	return sorted[sorted.length - 1].key;
 }
 
 function calculateSpecialness(selection) {
@@ -111,8 +104,7 @@ for (let i = 0; i < 6; i++) {
 		devised = [...devised, types[Math.floor(Math.random() * 6)].name];
 		continue;
 	}
-	GetNextWithWeights(devised[i - 1]);
-	devised = [...devised, types[Math.floor(Math.random() * 6)].name];
+	devised = [...devised, GetNextWithWeights(devised[i - 1])];
 }
 
 console.log(`Your set -> ${devised}`);
